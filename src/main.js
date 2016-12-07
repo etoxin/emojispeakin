7a6f9141ee98299ad3da84b809b0fd0e4cca8928
etoxin <adam@etoxin.net>
2016-12-07 21:06:29 +1100

    Added speech api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,52 @@
-import React from 'react';
-import {render} from 'react-dom';
-import HelloWorld from './HelloWorld.js';
+import _ from 'lodash';
 
-render(<HelloWorld/>, document.querySelector('#app'));
+/**
+ * @class EmojiSpeaking
+ */
+class EmojiSpeaking {
+    /**
+     * @constructor
+     */
+    constructor () {
+        console.log('Running');
+
+        this.transcript = "";
+        this.recognition = new webkitSpeechRecognition();
+        this.recognition.interimResults = true;
+        this.recognition.onstart = function(event){
+            console.log('onStart', event);
+        };
+        this.recognition.onresult = function (event) {
+            console.log('boom', event.results[0][0].transcript);
+            this.transcript = event.results[0][0].transcript;
+        };
+        this.recognition.onerror = function(event) {
+            console.log("Fuck", event);
+        };
+    }
+
+    /**
+     *
+     */
+    start () {
+        this.recognition.start();
+    }
+
+    /**
+     *
+     * @param text {string}
+     */
+    printToPage (text) {
+
+    }
+}
+
+class Page {
+    contructor () {
+        this.template = _.template('<div><&= text &></div>');
+    }
+}
+
+
+var app = new EmojiSpeaking();
+app.start();
