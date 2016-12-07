cb9beacd1c9c215a47588c73e1bce2ef28c21925
etoxin <adam@etoxin.net>
2016-12-07 21:45:30 +1100

    Added things

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ class EmojiSpeaking {
      * @constructor
      */
     constructor () {
-        console.log('Running');
+
+        // todo: no api, soz, not soz.
 
         this.transcript = "";
         this.recognition = new webkitSpeechRecognition();
@@ -17,8 +18,17 @@ class EmojiSpeaking {
             console.log('onStart', event);
         };
         this.recognition.onresult = function (event) {
-            console.log('boom', event.results[0][0].transcript);
+            console.log('Transcript', event.results[0][0].transcript);
             this.transcript = event.results[0][0].transcript;
+
+            var event = new CustomEvent("transcript", {
+                detail: {
+                    transcript: this.transcript,
+                    result: event
+                }
+            });
+
+            document.dispatchEvent(event);
         };
         this.recognition.onerror = function(event) {
             console.log("Fuck", event);
@@ -26,18 +36,47 @@ class EmojiSpeaking {
     }
 
     /**
-     *
+     * Start
      */
     start () {
         this.recognition.start();
     }
 
-    /**
-     *
-     * @param text {string}
-     */
-    printToPage (text) {
+    error() {
+        // todo: print to page
+    };
+}
 
+class TranslateToEmoji {
+    translate (event) {
+        var transcript = event.detail.transcript;
+
+        console.log(123);
+        if(_.isString(transcript)) {
+
+            // first split the transcript up into an array spliting spaces.
+            var transcriptArray = transcript.spilt(" ");
+
+            // them we go through each item and match up emoji's (to many)
+
+            // if many random that
+
+            // replace
+
+            // reconstruct
+
+            console.log(transcriptArray);
+
+        } else {
+            this.error();
+        }
+    }
+
+    constructor () {
+        document.addEventListener("transcript", function(event) {
+            console.log('TranslateToEmoji', event.detail.transcript);
+        });
+        document.addEventListener("transcript", this.translate);
     }
 }
 
@@ -49,4 +88,5 @@ class Page {
 
 
 var app = new EmojiSpeaking();
+var emojiTranslate = new TranslateToEmoji();
 app.start();
